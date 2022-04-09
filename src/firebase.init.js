eef822705361a5d85fb3d280ed7b5e3d339f98e3
MdMostaqAhmed <md.mostaqahmed.bd@gmail.com>
2022-04-09 21:38:23 +0600

    router and firebade npm install

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA32Ksxf5fJojCpNSIi7wULVStBCQHonYg",
+  authDomain: "router-firebase-integrat-a7ae5.firebaseapp.com",
+  projectId: "router-firebase-integrat-a7ae5",
+  storageBucket: "router-firebase-integrat-a7ae5.appspot.com",
+  messagingSenderId: "682952035213",
+  appId: "1:682952035213:web:5074230f082db097e88bc4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
